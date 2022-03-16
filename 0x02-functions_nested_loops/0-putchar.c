7e0d0e293969b684bfb1abe0d53efa1a5d1b2a84
MiyomaBon <glomiyoma@gmail.com>
2022-03-16 12:33:01 +0300

    grit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+ * main - Putchar
+ *
+ * _putchar print the characters given
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	_putchar('_');
@@ -16,6 +18,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
